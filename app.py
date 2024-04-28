6e47226af209caecbb5ba6954ede33a0be3d1e3b
rithinprem <147548144+rithinprem@users.noreply.github.com>
2024-04-28 17:06:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,Response
+from flask import Flask, render_template,request,Response,abort
 import plotly.express as px   #for plotting
 import plotly.io as pio
 import numpy as np
